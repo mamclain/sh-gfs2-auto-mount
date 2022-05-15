1e4fac859defacab406ee1a1a6fe86d6da63c27d
Mike Mclain <mamclain@gmail.com>
2022-05-15 05:50:49 -0400

    Initial commit with readme and code

--- README.md
+++ README.md
@@ -1,2 +1,129 @@
 # sh-gfs2-auto-mount
-A utility script and service to ensure gfs2 mounts get mounted on boot
+
+## About
+
+A utility script and service to ensure gfs2 mounts get mounted on boot [With a target of Debian based systems].
+
+This script checks that the necessary gfs2 perquisites are installed and configured and waits for `corosync` quorum, `dlm` startup, and `iscsi` device connection prior to in invoking a `gfs2`  mount. 
+
+## Prerequisite
+
+In order for this script to work you should have gfs2 installed and configured on your system
+
+Notable actions include:
+
+* Install and setup `open-iscsi` [*If `iscsi` is your backend*]
+* Install and setup `corosync`
+* Install and setup `dlm`
+* Install and setup `gfs2-utils`
+* Create a GFS2 partition
+* Test and ensure you can mount a gfs2 partition manually
+
+## Install GFS2 Auto Mount
+
+copy or link `gfs2_auto_mount.sh` to `/usr/local/bin`
+
+set permissions via ```chmod``` if needed
+
+```bash
+chmod 700 /usr/local/bin/gfs2_auto_mount.sh
+```
+
+You may also pick other locations and or permission configurations as desired. [optimal configurations here are still being evaluated]
+
+## GFS2 Auto Mount Input Commands
+
+| Command  | Description                                                  |
+| -------- | ------------------------------------------------------------ |
+| -d <arg> | The Input GFS2 Device.<br />Normally like `/dev/disk/by-path/ip-X.X.X.X:PORT-iscsi-iqn.YYYY-MM.DOMAIN.com:lun1-lun-1` |
+| -m <arg> | The Output Folder to Mount. <br />Normally `/mnt/your_folder_mount` |
+| -v       | Show Debug Output                                            |
+| -u       | Note: Requires -m <arg>,<br />used by `ExecStop` to signal that the gfs2 mount should be unmounted if mounted on `systemctl stop` event |
+| -h       | A Help Message                                               |
+
+## Create GFS2 Auto Mount Service
+
+I recommend using the name of the mount to conform to the `systemctl` standards.
+
+For example, if your want to mount the iscsi mount `dev/sdc` to the `/mnt/iso` folder name your service `mnt-iso.service` and create it via:
+
+```bash
+nano /etc/systemd/system/mnt-iso.service
+```
+
+your service file should look like this
+
+```bash
+[Unit]
+Description = Mount GFS2 share over iSCSI LUN For /mnt/iso
+Wants=open-iscsi.service dlm.service corosync.service
+After=open-iscsi.service dlm.service corosync.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=true
+ExecStart=/bin/bash -c "/usr/local/bin/gfs2_auto_mount.sh -d /dev/disk/by-path/ip-X.X.X.X:PORT-iscsi-iqn.YYYY-MM.DOMAIN.com:lun1-lun-1 -m /mnt/iso -d"
+ExecStop=/bin/bash -c "/usr/local/bin/gfs2_auto_mount.sh -m /mnt/iso -u -d"
+
+[Install]
+WantedBy=multi-user.target
+
+```
+
+Note: `Type=oneshot` and `Type=oneshot`can be replaced with `Type=forking` If desired.
+
+Note: you can remove the `-d` from the `Exec` lines above to disable the debug output in `journalctl`
+
+once saved reload `systemctl` via:
+
+```bash
+systemctl daemon-reload
+```
+
+Next test the service start via
+
+```bash
+systemctl start mnt-iso.service
+```
+
+Next check service status for errors, also check `journalctl` for service logs
+
+```bash
+systemctl status mnt-iso.service
+journalctl -u mnt-iso.service -b
+```
+
+if the start test is good then test the stop service via:
+
+```bash
+systemctl stop mnt-iso.service
+```
+
+And check the `status` log for errors
+
+Note: `dmesg` can be used to check for `gfs2` and `iscsi` errors
+
+Note, you need to get a `gfs2` mount working manually before attempting to use this script, this script assume you have your `corosync`, `dlm`, and `gfs2` mount configured correctly
+
+If both stop and start are working, enable the service
+
+```bash
+systemctl enable mnt-iso.service
+```
+
+and reboot.
+
+On reboot check the service status, and review `systemctl` load dependencies via
+
+```bash
+systemctl list-dependencies
+systemd-analyze critical-chain mnt-iso.service
+```
+
+For errors. Also check the mount via
+
+```bash
+lsblk
+```
+
+to ensure the mount is working.
---
+++ gfs2_auto_mount.sh
@@ -0,0 +1,297 @@
+#!/bin/bash
+# A servicectl service script to check the status of gfs2 and mount a target folder
+
+major_version=1
+minor_version=0
+
+# a flag for debug messages
+debug=1
+mode=1
+
+
+# debug print
+message () {
+  if [ "$debug" -eq 1 ]
+  then
+    echo "$1"
+  fi
+}
+
+# check to see if we have coresync
+have_corosync() {
+  corosync_system_status=$(systemctl status corosync 2>&1 >/dev/null | grep -q "not be found" && [ $? -eq 0 ] && echo "No" || echo "Yes")
+  if [ "$corosync_system_status" = "Yes" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+# corosync status check
+corosync_status() {
+  corosync_state=$(systemctl status corosync | grep Active: | awk '{print $2}')
+  if [ "$corosync_state" = "active" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+# corosync quorum check
+corosync_quorum() {
+  quorate_state=$(corosync-quorumtool | grep Quorate: | awk '{ print substr($0, index($0,$2)) }')
+  message "Quorate: $quorate_state"
+  if [ "$quorate_state" == "Yes" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+
+# check to see if we have dlm
+have_dlm(){
+  dlm_system_status=$(systemctl status dlm 2>&1 >/dev/null | grep -q "not be found" && [ $? -eq 0 ] && echo "No" || echo "Yes")
+  if [ "$dlm_system_status" = "Yes" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+# dlm status check
+dlm_status() {
+  dlm_state=$(systemctl status dlm | grep Active: | awk '{print $2}')
+  if [ "$dlm_state" = "active" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+
+# check to see if we have open-iscsi
+have_open_iscsi(){
+  open_iscsi_system_status=$(systemctl status open-iscsi 2>&1 >/dev/null | grep -q "not be found" && [ $? -eq 0 ] && echo "No" || echo "Yes")
+  if [ "$open_iscsi_system_status" = "Yes" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+# dlm status check
+open_iscsi_status() {
+  open_iscsi_state=$(systemctl status open-iscsi | grep Active: | awk '{print $2}')
+  if [ "$open_iscsi_state" = "active" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+
+# is block device status check
+block_device_exists() {
+  if [ -b "$1" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+# folder exists
+folder_exists() {
+  if [ -d "$1" ]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
+
+# entry point with args
+while getopts d:m:vhu flag
+do
+    case "${flag}" in
+        d) 
+          device=${OPTARG}
+        ;;
+        m)
+          mount=${OPTARG}
+        ;;
+        v)
+          debug=1
+        ;;
+        u)
+          mode=2
+        ;;
+        h)
+          echo "GFS2 Auto Mount V$major_version.$minor_version By Mike Mclain"
+          echo "-d    The Input GFS2 Device"
+          echo "-m    The Output Folder to Mount"
+          echo "-v    Show Debug to Output"
+          echo "-u    Used by execstop flag to unmount the gfs2 mount if mounted"
+          echo "-h    This Help Message"
+          exit
+        ;;
+    esac
+done
+
+# check if the user passed in a mount
+if [ -z "$mount" ]
+then
+  echo "No Mount Path Found"  
+  exit
+fi
+
+# check if mount folder exisits
+if folder_exists $mount -eq 0
+then
+  echo "Mount folder $mount does not exists"
+  exit
+fi
+
+
+# check if mount is mounted
+is_mountpoint=$(mountpoint $mount | grep not)
+if [ -z "$is_mountpoint" ]
+then
+  # if so, check mode to see if we are unmount or error
+  if [ "$mode" -eq 1 ]
+  then
+    echo "Mount folder $mount is already mounted"
+    exit
+  else
+    echo "Script called via -u, will unmount $mount"  
+    umount $mount
+    exit
+  fi
+else
+  # catch a non mount case of script stop mode
+  if [ "$mode" -eq 2 ]
+  then
+      echo "Script called via -u, Mount $mount was not mounted"  
+      exit
+  fi
+fi
+
+  
+# check if a user passed in a device
+if [ -z "$device" ]
+then
+  echo "No Device Path Found"  
+  exit
+fi
+
+
+# check for corosync
+check_corosync=$(which corosync-quorumtool)
+
+if [ -z "$check_corosync" ]
+then
+  echo "corosync-quorumtool not found"  
+  exit
+fi
+
+# check for dlm_tool
+check_dlm_tool=$(which dlm_tool)
+
+if [ -z "$check_dlm_tool" ]
+then
+  echo "dlm_tool not found"  
+  exit
+fi
+
+# check for iscsiadm
+check_iscsiadm=$(which iscsiadm)
+
+if [ -z "$check_iscsiadm" ]
+then
+  echo "iscsiadm not found"  
+  exit
+fi
+
+# check for service in systemctl for corosync
+if have_corosync -eq 0
+then
+  echo "Corosync not found in systemctl"
+  exit
+fi
+
+# check for service in systemctl for dlm
+if have_dlm -eq 0
+then
+  echo "dlm not found in systemctl"
+  exit
+fi
+
+# check for service in systemctl for open_iscsi
+if have_open_iscsi -eq 0
+then
+  echo "open_iscsi not found in systemctl"
+  exit
+fi
+
+
+# wait for corosync start
+message "Wait on Corosync Start"
+while corosync_status -eq 0;
+do
+ /usr/bin/sleep 5;
+done
+message "Corosync Is Online"
+
+# check for quorum
+message "Wait on Corosync Quorum"
+while corosync_quorum -eq 0;
+do
+ /usr/bin/sleep 5;
+done
+message "Corosync Quorum Online"
+
+# wait for dlm start
+message "Wait on dlm Start"
+while dlm_status -eq 0;
+do
+ /usr/bin/sleep 5;
+done
+message "dlm Is Online"
+
+# wait for open_iscsi start
+message "Wait on open iscsi Start"
+while open_iscsi_status -eq 0;
+do
+ /usr/bin/sleep 5;
+done
+message "open iscsi Is Online"
+
+
+# wait for the Device mount
+message "Wait on Device $device"
+while block_device_exists $device -eq 0;
+do
+ /usr/bin/sleep 5;
+done
+message "Device $device Is Online"
+
+
+
+# at this point lets try to mount
+message "Try to mount $device to $mount"
+mount $device $mount
+
+
+
+
+
+ 
